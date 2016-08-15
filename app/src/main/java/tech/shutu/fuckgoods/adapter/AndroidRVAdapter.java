5c81dd20d48e1296639f736cf822d4737df7307d
raomengyang <raomengyang@163.com>
2016-08-16 00:33:00 +0800

    Added item for AndroidRVAdapter

--- app/src/main/java/tech/shutu/fuckgoods/adapter/AndroidRVAdapter.java
+++ app/src/main/java/tech/shutu/fuckgoods/adapter/AndroidRVAdapter.java
@@ -1,36 +1,54 @@
 package tech.shutu.fuckgoods.adapter;
 
 import android.content.Context;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import tech.shutu.fuckgoods.R;
+import tech.shutu.fuckgoods.bean.AndroidBean;
 
 /**
  * Created by raomengyang on 8/15/16.
  */
-public class AndroidRVAdapter extends RecyclerView.Adapter {
+public class AndroidRVAdapter extends RecyclerView.Adapter<AndroidRVAdapter.ViewHolder> {
 
     private Context mContext;
+    private List<AndroidBean> androidBeanList = new ArrayList<>();
 
     public AndroidRVAdapter(Context context) {
         this.mContext = context;
     }
 
+    public void setDataToAdapter(List<AndroidBean> list) {
+        if (androidBeanList == null) androidBeanList = list;
+        else {
+            androidBeanList.clear();
+            androidBeanList.addAll(list);
+        }
+    }
+
+
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.)
-        return null;
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_android_rv, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-
+    public void onBindViewHolder(ViewHolder holder, int position) {
+//        AndroidBean bean = androidBeanList.get(position).getResults()
     }
+
 
     @Override
     public int getItemCount() {
@@ -38,4 +56,19 @@ public class AndroidRVAdapter extends RecyclerView.Adapter {
     }
 
 
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.tv_publish_time)
+        TextView tvPublishTime;
+        @BindView(R.id.tv_type)
+        TextView tvType;
+        @BindView(R.id.tv_author)
+        TextView tvAuthor;
+        @BindView(R.id.cv_android)
+        CardView cvAndroid;
+
+        ViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+        }
+    }
 }
